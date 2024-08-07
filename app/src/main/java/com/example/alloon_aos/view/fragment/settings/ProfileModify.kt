4d7344a22d06f9b1789fd3ee973e75b241382002
Seul Ah <90150969+Seul-Seul@users.noreply.github.com>
2024-08-07 13:05:22 +0900

    Feat: 프로필 수정 ui 및 플로우
    + 텍스트버튼 글자색 selector 추가, theme 수정 및 적용

--- app/src/main/java/com/example/alloon_aos/view/fragment/settings/ProfileModify.kt
+++ app/src/main/java/com/example/alloon_aos/view/fragment/settings/ProfileModify.kt
@@ -1,60 +1,41 @@
 package com.example.alloon_aos.view.fragment.settings
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.alloon_aos.R
+import com.example.alloon_aos.databinding.FragmentProfileModifyBinding
+import com.example.alloon_aos.databinding.FragmentUnSubscribeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileModify.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileModify : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentProfileModifyBinding
+    private lateinit var mContext: Context
+    // private val authViewModel by activityViewModels<AuthViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_modify, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_modify, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileModify.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileModify().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun moveFlag(i : Int){
+        when(i){
+            1 -> {
+                println("비밀번호 변경")
             }
+            2 -> {
+                view?.findNavController()?.navigate(R.id.action_profileModifyFragment_to_unSubscribeFragment)
+            }
+        }
     }
 }
