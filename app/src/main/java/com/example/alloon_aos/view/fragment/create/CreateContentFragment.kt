69f8c2c0e1820c97250c62c9943537c2292fd493
b22016645 <byeolstar12@naver.com>
2024-10-07 21:40:45 +0900

    Style: time picker 배경

--- app/src/main/java/com/example/alloon_aos/view/fragment/create/CreateContentFragment.kt
+++ app/src/main/java/com/example/alloon_aos/view/fragment/create/CreateContentFragment.kt
@@ -2,9 +2,12 @@ package com.example.alloon_aos.view.fragment.create
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -13,6 +16,8 @@ import com.example.alloon_aos.databinding.FragmentCreateContentBinding
 import com.example.alloon_aos.view.activity.CreateActivity
 import com.example.alloon_aos.view.ui.component.bottomsheet.TimeBottomSheet
 import com.example.alloon_aos.view.ui.component.bottomsheet.TimeBottomSheetInterface
+import com.example.alloon_aos.view.ui.util.KeyboardListener
+import com.example.alloon_aos.view.ui.util.OnKeyboardVisibilityListener
 import com.example.alloon_aos.viewmodel.CreateViewModel
 import java.text.DecimalFormat
 
@@ -48,6 +53,31 @@ class CreateContentFragment : Fragment(), TimeBottomSheetInterface {
             TimeBottomSheet(mContext,createViewModel,this)
                 .show(activity?.supportFragmentManager!!, "bottomList")
         }
+
+        binding.root.setOnClickListener {
+            if (activity != null && requireActivity().currentFocus != null) {
+                val inputManager: InputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+
+        KeyboardListener.setKeyboardVisibilityListener(binding.root,object :
+            OnKeyboardVisibilityListener {
+            override fun onVisibilityChanged(visible: Boolean) {
+                if (visible)    binding.contentEdittext.background = mContext.getDrawable(R.drawable.textarea_line_focus)
+                else    binding.contentEdittext.background = mContext.getDrawable(R.drawable.textarea_line_default)
+            }
+        })
+
+        binding.contentEdittext.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.numTextview.setText("${s!!.length}/120")
+            }
+        })
     }
 
     override fun onClickSelectButton(time: Int) {
