cf28a2aa61138fbac0f74cbac5f5aace12dfd79b
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2024-10-26 17:52:35 +0900

    Style: 챌린지 라운드 크롭 처리 변경
    card_shape수정

--- app/src/main/java/com/example/alloon_aos/view/adapter/FeedInAdapter.kt
+++ app/src/main/java/com/example/alloon_aos/view/adapter/FeedInAdapter.kt
@@ -5,10 +5,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.example.alloon_aos.R
 import com.example.alloon_aos.databinding.ItemFeedCommentBinding
 import com.example.alloon_aos.databinding.ItemFeedDefaultInBinding
 import com.example.alloon_aos.view.ui.component.dialog.FeedDetailDialog
+import com.example.alloon_aos.view.ui.util.RoundedCornersTransformation
 import com.example.alloon_aos.viewmodel.Comment
 import com.example.alloon_aos.viewmodel.FeedInItem
 interface OnFeedDeletedListener {
@@ -36,6 +38,7 @@ class FeedInAdapter(
                 Glide
                     .with(holder.itemView.context)
                     .load(url)
+                    .transform(CenterCrop(), RoundedCornersTransformation(20f, 68f))
                     .into(binding.imgView)
 
                 binding.feed.setOnClickListener {
