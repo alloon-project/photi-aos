7c442d0d173bb97f646b8dc126ca96eaa07ef78f
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2025-07-04 23:40:39 +0900

    Android15대응

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.photi.aos"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.0.1"
 
