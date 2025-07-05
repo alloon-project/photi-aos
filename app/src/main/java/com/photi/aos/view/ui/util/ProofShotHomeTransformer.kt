7f8438de16e97b2ef00bfe15f4bf55370dbcab46
b22016645 <byeolstar12@naver.com>
2025-07-05 20:29:49 +0900

    fix: 홈/챌린지 인증 카드 마진 및 간격 수정

--- app/src/main/java/com/photi/aos/view/ui/util/ProofShotHomeTransformer.kt
+++ app/src/main/java/com/photi/aos/view/ui/util/ProofShotHomeTransformer.kt
@@ -3,15 +3,12 @@ import android.view.View
 import androidx.viewpager2.widget.ViewPager2
 
 class ProofShotHomeTransformer : ViewPager2.PageTransformer {
-    private val pageMarginPx = 296.dpToPx()
+    private val pageMarginPx = 16.dpToPx()
 
     override fun transformPage(page: View, position: Float) {
-        val pageWidth = page.width
-
-        // 페이지의 위치 조정
-        page.translationX = -pageWidth * position + pageMarginPx * position
-
+        page.translationX = -pageMarginPx * position
     }
+
     fun Int.dpToPx(): Int {
         val density = Resources.getSystem().displayMetrics.density
         return (this * density).toInt()
