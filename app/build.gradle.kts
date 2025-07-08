ff70ca510ada4ba0cd8e05cfa7fd0f6734a03b55
b22016645 <byeolstar12@naver.com>
2025-07-08 20:06:15 +0900

    fix: 인증 올리기 버튼 아이콘 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.0")
 
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
