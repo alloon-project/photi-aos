887574c5e95ce8ee7b3b1eb0a67876b7c994abb5
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2025-10-05 21:00:13 +0900

    Chore: 빌드 번호 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.photi.aos"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
