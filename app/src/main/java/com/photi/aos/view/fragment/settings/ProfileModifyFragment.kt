bb3251d1a930a4dbfefd282f54a43d894d9602ec
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2026-03-01 23:00:40 +0900

    feat: OAuth 회원탈퇴 제한 토스트 추가
    - 프로필 수정 화면에서 회원탈퇴 클릭 시 OAuth 로그인 유저는 탈퇴 화면 진입을 막고 안내 토스트 노출
    - TokenManager의 OAuthTokenSet 존재 여부로 OAuth 계정 여부 판단
    - 이메일 가입 유저는 기존 회원탈퇴 플로우 그대로 유지

--- app/src/main/java/com/photi/aos/view/fragment/settings/ProfileModifyFragment.kt
+++ app/src/main/java/com/photi/aos/view/fragment/settings/ProfileModifyFragment.kt
@@ -18,7 +18,10 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.photi.aos.MyApplication
 import com.photi.aos.R
+import com.photi.aos.data.storage.SharedPreferencesManager
+import com.photi.aos.data.storage.TokenManager
 import com.photi.aos.databinding.FragmentProfileModifyBinding
 import com.photi.aos.view.activity.SettingsActivity
 import com.photi.aos.view.ui.component.toast.CustomToast
@@ -27,12 +30,16 @@ import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
+import javax.inject.Inject
+
 
 class ProfileModifyFragment : Fragment() {
     private lateinit var binding: FragmentProfileModifyBinding
     private val settingsViewModel by activityViewModels<SettingsViewModel>()
     private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
     private var isModified = false
+
+    private val tokenManager = TokenManager(MyApplication.mySharedPreferences)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -147,6 +154,17 @@ class ProfileModifyFragment : Fragment() {
             }
 
             2 -> {
+                //TODO 임시
+                val oauthSet = tokenManager.getOAuthTokenSet()
+                Log.d("ProfileModifyFragment","provider :${oauthSet?.provider} idToken :${oauthSet?.idToken}")
+                if (oauthSet != null) {
+                    CustomToast.createToast(
+                        activity,
+                        "기능을 준비 중입니다. 고객센터로 문의해 주세요.",
+                        "circle"
+                    )?.show()
+                    return
+                }
                 view?.findNavController()
                     ?.navigate(R.id.action_profileModifyFragment_to_unSubscribeFragment)
             }
