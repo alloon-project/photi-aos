781b1fc3c98c5f83300726f575bf4b5ce8c8039c
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2026-03-04 21:01:13 +0900

    chore: 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 8
-        versionName = "2.0.0"
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
