f1fc314c3effef743fdd63f62fefd90460a35fcf
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2026-08-10 15:45:21 +0900

    Chore: 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.photi.aos"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.1.0"
+        versionCode = 13
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
